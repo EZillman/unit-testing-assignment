68a63f2c71c09142e3b90ea3ab491f969f6dd9ce
EZillman <evelina.zillman@medieinstitutet.se>
2023-01-26 14:51:54 +0100

    second test on displayerror

--- src/_test_/main.test.ts
+++ src/_test_/main.test.ts
@@ -24,4 +24,19 @@ describe('should add or remove css class on div depending on argument value', ()
         let result = document.getElementById('error') as HTMLDivElement;
         expect(result.classList.contains("show")).toBe(true);
     });
+
+    test('should remove class "show" if false', () => {
+
+        // Arrange
+        let errorText = 'An error has occured';
+        document.body.innerHTML = `
+        <div id="error" class="error" ></div>`;
+
+        // Act
+        displayError(errorText, false);
+
+        // Assert
+        let result = document.getElementById('error') as HTMLDivElement;
+        expect(result.classList.contains("show")).toBe(false);
+    });
 });
